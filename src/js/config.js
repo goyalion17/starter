091a7cf9551c1320c048f3bef05327f8bb2ab1d2
Irina Saveleva <goyalion17@gmail.com>
2023-12-22 15:17:37 +0100

    forkify

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const REC_PER_PAGE = 10;
+export const KEY = 'eea79029-efcc-4637-b8e7-73bbb2b8323e';
+export const MODAL_CLOSE_SEC = 2.5;
